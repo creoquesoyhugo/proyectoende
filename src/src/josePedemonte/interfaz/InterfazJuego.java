4c175b88ab0742c3d1fa81d6fa2e393063703bbb
jose <jose@gmail.com>
2025-05-20 16:35:12 +0200

    agregando la logica de la InterfazJuego

--- src/src/josePedemonte/interfaz/InterfazJuego.java
+++ src/src/josePedemonte/interfaz/InterfazJuego.java
@@ -1,8 +1,23 @@
 package josePedemonte.interfaz;
 
+import java.util.Scanner;
+
 import static josePedemonte.interfaz.PantallaCarga.esperar;
 
 public class InterfazJuego {
 
+    private Scanner scanner;
 
+    public InterfazJuego() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void mostrarPantallaCarga() {
+        System.out.println("Cargando juego...");
+        for (int i = 0; i <= 100; i++) {
+            esperar();
+            System.out.print("\rProgreso: " + i + "%");
+        }
+        System.out.println("\n¡Carga completa!");
+    }
 }
