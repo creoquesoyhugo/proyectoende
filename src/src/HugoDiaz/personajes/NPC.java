2cea9a0a40356a77d05c6539f2d6896dd5aae323
hugi <hugodiazcv@gmail.com>
2025-05-20 13:19:37 +0200

    cuarto commit de la clase NPC

---
+++ src/src/HugoDiaz/personajes/NPC.java
@@ -0,0 +1,15 @@
+package HugoDiaz.personajes;
+
+public class NPC extends Personaje {
+    private String dialogo;
+
+    public NPC(String nombre, String dialogo) {
+        super(nombre, 1, 100); // NPCs tienen nivel y salud fijos
+        this.dialogo = dialogo;
+    }
+
+    @Override
+    public void habilidadEspecial() {
+        System.out.println(getNombre() + " dice: \"" + dialogo + "\"");
+    }
+}
