06926675b108904626e206a46cf8a1d5146ba312
Diego Berechet <dberechet92@gmail.com>
2025-05-20 12:37:41 +0200

    Revert "Creacion Clase Defensa"
    This reverts commit cf5f2bfb50ee130d8e20428f6fcf25c2de085df0.

--- src/src/com/grupoHugo/DiegoBerechet/combate/Ataque.java
+++ src/src/com/grupoHugo/DiegoBerechet/combate/Ataque.java
@@ -1,13 +1,5 @@
 package com.grupoHugo.DiegoBerechet.combate;
 
 public class Ataque {
-    private int daño;
 
-    public Ataque(int daño) {
-        this.daño = daño;
-    }
-
-    public void ejecutarAtaque() {
-        System.out.println("Ataque ejecutado con " + daño + " de daño.");
-    }
 }
