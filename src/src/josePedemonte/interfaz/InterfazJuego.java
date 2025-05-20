e3b2bc9985519583b48fa131f323813a419aad2a
jose <jose@gmail.com>
2025-05-20 16:44:37 +0200

    corrección del juego

--- src/src/josePedemonte/interfaz/InterfazJuego.java
+++ src/src/josePedemonte/interfaz/InterfazJuego.java
@@ -4,22 +4,24 @@ import java.util.Scanner;
 
 public class InterfazJuego {
 
-    private final Scanner scanner;
+    private Scanner scanner;
     private PantallaCarga pantallaCarga;
     private MenuPrincipal menu;
     private ConsolaMensajes consola;
 
     public InterfazJuego() {
-        scanner = new Scanner(System.in);
-        pantallaCarga = new PantallaCarga();
-        menu = new MenuPrincipal(scanner);
-        consola = new ConsolaMensajes();
+        this.scanner = new Scanner(System.in);
+        this.pantallaCarga = new PantallaCarga();
+        this.menu = new MenuPrincipal(scanner);
+        this.consola = new ConsolaMensajes();
     }
 
     public void iniciar() {
         pantallaCarga.mostrar();
         consola.mostrarBienvenida();
         menu.mostrar();
-    }
 
+        // Cierre del scanner si ya no se va a usar más en el resto del juego
+        scanner.close();
+    }
 }
