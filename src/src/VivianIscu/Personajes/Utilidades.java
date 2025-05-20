2a01c6f16cc3ac3d0312e5961530c685997a763f
ISK <vivianiscu12345@gmail.com>
2025-05-20 13:09:53 +0200

    octavo commit: "Utilidad".

---
+++ src/src/VivianIscu/Personajes/Utilidades.java
@@ -0,0 +1,7 @@
+package VivianIscu.Personajes;
+
+public class Utilidades {
+    public static void mostrarSalud(Personaje p) {
+        System.out.println(p.getNombre() + " tiene " + p.getSalud() + " de salud.");
+    }
+}
