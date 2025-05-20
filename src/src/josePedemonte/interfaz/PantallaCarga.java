c745dcfde8c3616eef77da6aa067d19b841ac114
jose <jose@gmail.com>
2025-05-20 16:27:52 +0200

    añadí la logica de PantallaCarga para que simule la carga

--- src/src/josePedemonte/interfaz/PantallaCarga.java
+++ src/src/josePedemonte/interfaz/PantallaCarga.java
@@ -1,4 +1,22 @@
 package josePedemonte.interfaz;
 
 public class PantallaCarga {
+
+    public static void main(String[] args) {
+        System.out.println("Cargando...");
+
+        for (int i = 0; i <= 100; i++) {
+            esperar(); // simula tiempo de espera
+            System.out.print("\rProgreso: " + i + "%");
+        }
+
+        System.out.println("\n¡Carga completada!");
+    }
+
+    public static void esperar() {
+        for (long i = 0; i < 500_000; i++) {
+            // No hacer nada (solo perder tiempo)
+        }
+    }
 }
+
