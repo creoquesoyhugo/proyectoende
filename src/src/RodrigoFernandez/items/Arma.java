bb14685d755946042030a2fa2b208f32d5b1c1c4
Korosensei22 <rodrigofernandezcoenjo@gmail.com>
2025-05-20 12:52:32 +0200

    Segundo commit en el que se crea la clase de armas

---
+++ src/src/RodrigoFernandez/items/Arma.java
@@ -0,0 +1,23 @@
+package RodrigoFernandez.items;
+
+
+public class Arma extends ItemCombate {
+    private int danio;
+    private String tipo; // Espada, Arco, Hacha, etc.
+
+    public Arma(String nombre, int durabilidad, int nivelRequerido, int danio, String tipo) {
+        super(nombre, durabilidad, nivelRequerido);
+        this.danio = danio;
+        this.tipo = tipo;
+    }
+
+    @Override
+    public void usar() {
+        System.out.println("¡Atacas con " + getNombre() + " causando " + danio + " de daño!");
+        reducirDurabilidad(1);
+    }
+
+    public int getDanio() {
+        return danio;
+    }
+}
