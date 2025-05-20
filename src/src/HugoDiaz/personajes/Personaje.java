0130ff955539ac7cc549f066e4601b58cbbc3c98
hugi <hugodiazcv@gmail.com>
2025-05-20 13:17:51 +0200

    primer commit de la clase personaje

---
+++ src/src/HugoDiaz/personajes/Personaje.java
@@ -0,0 +1,27 @@
+package HugoDiaz.personajes;
+
+
+public abstract class Personaje {
+    private String nombre;
+    private int nivel;
+    private int salud;
+
+    public Personaje(String nombre, int nivel, int salud) {
+        this.nombre = nombre;
+        this.nivel = nivel;
+        this.salud = salud;
+    }
+
+    // Métodos comunes
+    public void atacar() {
+        System.out.println(nombre + " ataca!");
+    }
+
+    public abstract void habilidadEspecial();
+
+    // Getters y Setters
+    public String getNombre() { return nombre; }
+    public int getNivel() { return nivel; }
+    public int getSalud() { return salud; }
+    public void setSalud(int salud) { this.salud = salud; }
+}
