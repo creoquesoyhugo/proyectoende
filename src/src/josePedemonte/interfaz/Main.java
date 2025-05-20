aec6bf222f52cb3fe1359405a706a79dd86d7239
jose <jose@gmail.com>
2025-05-20 16:43:46 +0200

    Metodo Main para probar

---
+++ src/src/josePedemonte/interfaz/Main.java
@@ -0,0 +1,8 @@
+package josePedemonte.interfaz;
+
+public class Main {
+    public static void main(String[] args) {
+        InterfazJuego interfaz = new InterfazJuego();
+        interfaz.iniciar();
+    }
+}
