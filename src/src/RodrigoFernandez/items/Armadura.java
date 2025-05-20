e1e9b93d69ae06a6b37eaf5caff997278573b833
Korosensei22 <rodrigofernandezcoenjo@gmail.com>
2025-05-20 12:53:04 +0200

    Tercer commit en el que se crea la clase de las armaduras

---
+++ src/src/RodrigoFernandez/items/Armadura.java
@@ -0,0 +1,24 @@
+package RodrigoFernandez.items;
+
+
+
+public class Armadura extends ItemCombate {
+    private int defensa;
+    private String material; // Cuero, Acero, Mágico, etc.
+
+    public Armadura(String nombre, int durabilidad, int nivelRequerido, int defensa, String material) {
+        super(nombre, durabilidad, nivelRequerido);
+        this.defensa = defensa;
+        this.material = material;
+    }
+
+    @Override
+    public void usar() {
+        System.out.println("¡Te proteges con " + getNombre() + " (" + material + ")!");
+        reducirDurabilidad(1);
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+}
