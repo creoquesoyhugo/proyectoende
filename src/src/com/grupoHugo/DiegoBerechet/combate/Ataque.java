cf5f2bfb50ee130d8e20428f6fcf25c2de085df0
Diego Berechet <dberechet92@gmail.com>
2025-05-20 12:33:35 +0200

    Creacion Clase Defensa

--- src/src/com/grupoHugo/DiegoBerechet/combate/Ataque.java
+++ src/src/com/grupoHugo/DiegoBerechet/combate/Ataque.java
@@ -1,5 +1,13 @@
 package com.grupoHugo.DiegoBerechet.combate;
 
 public class Ataque {
+    private int daño;
 
+    public Ataque(int daño) {
+        this.daño = daño;
+    }
+
+    public void ejecutarAtaque() {
+        System.out.println("Ataque ejecutado con " + daño + " de daño.");
+    }
 }
