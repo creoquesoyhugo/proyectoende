924d2444d86a2859a0e203041892c32281040e3d
ISK <vivianiscu12345@gmail.com>
2025-05-20 13:08:03 +0200

    quinto commit: "Hechizo".

---
+++ src/src/VivianIscu/Personajes/Hechizo.java
@@ -0,0 +1,19 @@
+package VivianIscu.Personajes;
+
+public class Hechizo {
+    private String nombre;
+    private int poder;
+
+    public Hechizo(String nombre, int poder) {
+        this.nombre = nombre;
+        this.poder = poder;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPoder() {
+        return poder;
+    }
+}
