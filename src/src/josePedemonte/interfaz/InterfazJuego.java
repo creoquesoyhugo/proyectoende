ff2a070c3ff1f84d4904df0dc735610b2f4feda5
jose <jose@gmail.com>
2025-05-20 16:40:23 +0200

    Clase principal que inicia la interacción del usuario con el juego.

--- src/src/josePedemonte/interfaz/InterfazJuego.java
+++ src/src/josePedemonte/interfaz/InterfazJuego.java
@@ -2,22 +2,24 @@ package josePedemonte.interfaz;
 
 import java.util.Scanner;
 
-import static josePedemonte.interfaz.PantallaCarga.esperar;
-
 public class InterfazJuego {
 
-    private Scanner scanner;
+    private final Scanner scanner;
+    private PantallaCarga pantallaCarga;
+    private MenuPrincipal menu;
+    private ConsolaMensajes consola;
 
     public InterfazJuego() {
         scanner = new Scanner(System.in);
+        pantallaCarga = new PantallaCarga();
+        menu = new MenuPrincipal(scanner);
+        consola = new ConsolaMensajes();
     }
 
-    public void mostrarPantallaCarga() {
-        System.out.println("Cargando juego...");
-        for (int i = 0; i <= 100; i++) {
-            esperar();
-            System.out.print("\rProgreso: " + i + "%");
-        }
-        System.out.println("\n¡Carga completa!");
+    public void iniciar() {
+        pantallaCarga.mostrar();
+        consola.mostrarBienvenida();
+        menu.mostrar();
     }
+
 }
